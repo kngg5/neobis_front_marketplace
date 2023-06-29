37ecc316f525bae265ce9311c529fa9069fced77
kngg5 <yasinmamatov12@gmail.com>
2023-06-29 17:25:04 +0600

    Register file is created

---
+++ src/components/log in/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div class="registration_section">
+        <div class="background_img">
+            <img src="public/images/image-1mrt.jpg" alt="background image"/>
+        </div>
+        <div class="registration_form">
+            <input type="text" name="registration_form__user-name" id="registration_form__user-name"/>
+            
+        </div>
+    </div>
+  );
+}
+
+export default Register;
